938aadab96b614111bf77f5008bde1f1d37d8a26
kgurindagunta <121335368+kgurindagunta@users.noreply.github.com>
2025-11-22 18:41:11 +0530

    Update AnnotationTransformer.java

--- src/main/java/utility/AnnotationTransformer.java
+++ src/main/java/utility/AnnotationTransformer.java
@@ -10,7 +10,7 @@ public class AnnotationTransformer implements IAnnotationTransformer {
 
 	@Override
 	public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub12
 		annotation.setRetryAnalyzer(RetryAnalyzer.class);
 	}
 
